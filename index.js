d192b3d2a83c2ea3a00605322eb23950c2fafa30
Hugo Müller-Downing <hugo@localz.co>
2017-03-07 12:30:37 +1100

    Add support for objects in arrays (including nested) ✨

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const replaceRefs = (obj, searchStr, replaceStr) => {
-  return Object.assign(
-    {},
-    ...Object.keys(obj).map(k => {
-      if (typeof obj[k] === 'object') {
-        return {[k]: replaceRefs(obj[k], searchStr, replaceStr)};
-      }
-      if (k === searchStr) {
-        return {[replaceStr]: obj[k]};
-      }
-      return {[k]: obj[k]};
-    })
-  );
+  if (Array.isArray(obj)) {
+    return obj.map(k => replaceRefs(k, searchStr, replaceStr));
+  } else if (typeof obj === 'object') {
+    return Object.assign(
+      {},
+      ...Object.keys(obj).map(k => {
+        if (typeof obj[k] === 'object') {
+          if (k === searchStr) {
+            return {[replaceStr]: replaceRefs(obj[k], searchStr, replaceStr)};
+          }
+          return {[k]: replaceRefs(obj[k], searchStr, replaceStr)};
+        }
+        if (k === searchStr) {
+          return {[replaceStr]: obj[k]};
+        }
+        return {[k]: obj[k]};
+      })
+    );
+  }
 };
 
 exports.replaceRefsJSON = obj => replaceRefs(obj, '$ref', '_ref');
--- test.js
+++ test.js
@@ -30,3 +30,77 @@ test('Replace Mongo $refs', t => {
   const replaced = replaceRefsMongo(testMongo);
   t.deepEqual(replaced, testJSON);
 });
+
+const testJSONArray = {
+  $ref: 'test',
+  test: [
+    {
+      $ref: 'test',
+      something: 'else'
+    }
+  ]
+};
+
+const testMongoArray = {
+  _ref: 'test',
+  test: [
+    {
+      _ref: 'test',
+      something: 'else'
+    }
+  ]
+};
+
+test('Replace JSON $refs when using arrays', t => {
+  const replaced = replaceRefsJSON(testJSONArray);
+  t.deepEqual(replaced, testMongoArray);
+});
+
+test('Replace Mongo $refs when using arrays', t => {
+  const replaced = replaceRefsMongo(testMongoArray);
+  t.deepEqual(replaced, testJSONArray);
+});
+
+const testJSONNestedArray = {
+  $ref: 'test',
+  test: [
+    {
+      $ref: [
+        {
+          $ref: 'test'
+        },
+        {
+          $ref: 'test'
+        }
+      ],
+      something: 'else'
+    }
+  ]
+};
+
+const testMongoNestedArray = {
+  _ref: 'test',
+  test: [
+    {
+      _ref: [
+        {
+          _ref: 'test'
+        },
+        {
+          _ref: 'test'
+        }
+      ],
+      something: 'else'
+    }
+  ]
+};
+
+test('Replace JSON $refs when using nested arrays', t => {
+  const replaced = replaceRefsJSON(testJSONNestedArray);
+  t.deepEqual(replaced, testMongoNestedArray);
+});
+
+test('Replace Mongo $refs when using nested arrays', t => {
+  const replaced = replaceRefsMongo(testMongoNestedArray);
+  t.deepEqual(replaced, testJSONNestedArray);
+});
